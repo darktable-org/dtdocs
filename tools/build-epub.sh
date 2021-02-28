e3748a1d568dcd1f175b1c3bb7675f79e39dfa9b
Chris.Elston <chris.elston@blueyonder.co.uk>
2021-02-28 13:03:02 +0000

    epub front cover and metadata

--- tools/build-epub.sh
+++ tools/build-epub.sh
@@ -5,7 +5,6 @@ rm -rf public darktable.epub
 hugo -v --config config-epub.yaml
 
 cd public
-mkdir OEBPS
 
 mv darkroom guides-tutorials lighttable lua map module-reference overview preferences-settings print slideshow special-topics tethering OEBPS
 mv *.html content.opf toc.xhtml toc.ncx index.html style.css darktable-logo* OEBPS
