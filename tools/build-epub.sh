bccb4041e3f4a48a942c6a7bbf06bf6c5d845f7f
Chris.Elston <chris.elston@blueyonder.co.uk>
2021-02-28 10:32:45 +0000

    fix epub links

--- tools/build-epub.sh
+++ tools/build-epub.sh
@@ -8,6 +8,6 @@ cd public
 mkdir OEBPS
 
 mv darkroom guides-tutorials lighttable lua map module-reference overview preferences-settings print slideshow special-topics tethering OEBPS
-mv content.opf toc.xhtml toc.ncx index.html style.css darktable-logo* OEBPS
+mv *.html content.opf toc.xhtml toc.ncx index.html style.css darktable-logo* OEBPS
 
 zip -rX ../darktable.epub mimetype OEBPS META-INF
