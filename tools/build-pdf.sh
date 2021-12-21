2a007ddceaada8fdb9d3911fe4ca1121d8e428df
Chris Elston <chris.elston@blueyonder.co.uk>
2021-12-21 17:41:02 +0000

    remove old epub/pdf scripts and rename new ones

--- tools/build-pdf.sh
+++ tools/build-pdf.sh
@@ -4,11 +4,23 @@ OUTPUT=pdf
 HUGO_DIR=public-${OUTPUT}
 HUGO_CONFIG=config-${OUTPUT}.yaml
 
+# Get a list of languages
+languages=`find po -name '*.po' | cut -d . -f 2 | sort -u`
+# convert newlines to spaces
+languages=`echo $languages`
+
 rm -r ${HUGO_DIR}
 
 hugo serve --verbose --config ${HUGO_CONFIG} --bind 127.0.0.1 --port 1313 --disableFastRender -d ${HUGO_DIR} &
 sleep 30
-weasyprint -v http://127.0.0.1:1313/dtdocs/index.html ./public/darktable_user_manual.pdf
+mkdir -p ./public/en
+weasyprint -v http://127.0.0.1:1313/dtdocs/en/index.html ./public/en/darktable_user_manual.pdf
+for language in $languages
+do
+  echo $language
+  mkdir -p ./public/$language
+  weasyprint -v http://127.0.0.1:1313/dtdocs/$language/index.html ./public/$language/darktable_user_manual.pdf
+done
 pkill hugo
 
 rm -r ${HUGO_DIR}
